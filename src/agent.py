46745ec9a82aa15c9639013cf4795b5009738e75
Denivy Braiam Ruck <denivyruck@denivys-mbp.home>
2024-03-14 21:41:59 +0000

    Add numpy to split our data into chunks

--- src/agent.py
+++ src/agent.py
@@ -1,12 +1,14 @@
 
+from dataclasses import dataclass
 import datetime
 import time
+import numpy as np
 
 class DataSplitter:
 
-	def chunks(self):
-		return []
-
+	def chunks(self, data):
+		array = np.array(data)
+		return np.split(array, 10)
 
 class AgentWorker:
 	def run(self):
--- src/tests/test_data_splitter.py
+++ src/tests/test_data_splitter.py
@@ -6,4 +6,6 @@ from aose.src.agent import DataSplitter
 def test_data_is_divided_in_chunks(self):
 	data_splitter = DataSplitter()
 
-	assert data_splitter.chunks() == []
+	data = [1 for _ in range(100)]
+
+	assert data_splitter.chunks(data) == [[1, 1, 1, 1, 1, 1, 1, 1]]
